72d354edcc2b8ace2accc18c707814766a4a3522
iolinpic <iolinpic@gmail.com>
2025-03-20 19:51:57 +0300

    test

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/grpc/HubRouterGrpcClient.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/grpc/HubRouterGrpcClient.java
@@ -3,14 +3,14 @@ package ru.yandex.practicum.grpc;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.grpc.telemetry.event.DeviceActionRequest;
-import ru.yandex.practicum.grpc.telemetry.hubRouter.HubRouterControllerGrpc;
+import ru.yandex.practicum.grpc.telemetry.hubRouter.HubRouterControllerGrpc.HubRouterControllerBlockingStub;
 
 
 @Service
 public class HubRouterGrpcClient {
-    private final HubRouterControllerGrpc.HubRouterControllerBlockingStub hubRouterClient;
+    private final HubRouterControllerBlockingStub hubRouterClient;
 
-    public HubRouterGrpcClient(@GrpcClient("hub-router") HubRouterControllerGrpc.HubRouterControllerBlockingStub hubRouterClient) {
+    public HubRouterGrpcClient(@GrpcClient("hub-router") HubRouterControllerBlockingStub hubRouterClient) {
         this.hubRouterClient = hubRouterClient;
     }
 
